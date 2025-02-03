133bb9282ec954446723b7cd6bded0cd07a7fee6
Davidd256 <davidd256@gmail.com>
2025-02-04 01:25:10 +0300

    Редактирование ветки Branch2.

---
+++ OG04-1.py
@@ -0,0 +1,11 @@
+def calc(a, b):
+    print(f'a = {a}, b = {b}')
+    print(f'a + b = {a + b}')
+    print(f'a - b = {a - b}')
+    print(f'a * b = {a * b}')
+    print(f'a / b = {a / b}')
+
+
+a = 5
+b = 10
+calc(a, b);
